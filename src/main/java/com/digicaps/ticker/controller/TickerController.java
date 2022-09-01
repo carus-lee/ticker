a5711f6ccfbc397594d59c82794c55ec2a2c77f9
DESKTOP-SRKSIJD\user <leec8.2019@gmail.com>
2022-09-01 18:51:30 +0900

    긴급재난자막 API연동 진행중 - 파일조회시 한글깨짐 처리/파일저장 초안

--- src/main/java/com/digicaps/ticker/controller/TickerController.java
+++ src/main/java/com/digicaps/ticker/controller/TickerController.java
@@ -6,10 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.util.List;
 
@@ -17,20 +15,22 @@ import java.util.List;
 @RestController
 public class TickerController 
 {
-	private static final String USER_DIR = "C:\\home\\skylife\\input";
+	private static final String FILE_LOAD_DIR = "C:\\home\\skylife\\input";
+	private static final String FILE_SAVE_DIR = "C:\\home\\skylife\\output";
 	private WatchKey watchKey;
 
 	@PostConstruct
-	public void init() throws IOException
+	public void watchInit() throws IOException
 	{
 		//watchService 생성
 		WatchService watchService = FileSystems.getDefault().newWatchService();
 
-		log.info("디렉토리 경로 = {}", USER_DIR);
+		log.info("Static Directroy = {}", FILE_LOAD_DIR);
+//		log.info("property Directroy = {}", testDir);
 		//경로 생성
-		Path path = Paths.get(USER_DIR);
+		Path path = Paths.get(FILE_LOAD_DIR);
 
-		//해당 디렉토리 경로에 와치서비스와 이벤트 등록
+		//해당 디렉토리 경로에 와치서비스와 이벤트 등록 (프로퍼티로 이벤트 등록)
 		path.register(watchService, 
 				StandardWatchEventKinds.ENTRY_CREATE, 
 				StandardWatchEventKinds.ENTRY_DELETE, 
@@ -40,8 +40,9 @@ public class TickerController
 		Thread thread = new Thread(()-> {
 			while(true)
 			{
+				log.info("================= watchService Thread START =================");
 				try {
-					watchKey = watchService.take();//이벤트가 오길 대기(Blocking)
+					watchKey = watchService.take();//이벤트 대기(Blocking)
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
@@ -49,22 +50,21 @@ public class TickerController
 				List<WatchEvent<?>> events = watchKey.pollEvents(); //이벤트들을 가져옴
 				for(WatchEvent<?> event : events)
 				{
-					//이벤트 종류
-					WatchEvent.Kind<?> kind = event.kind();
-					//경로
-					Path paths = (Path)event.context();
+					WatchEvent.Kind<?> kind = event.kind(); //이벤트 종류
+					Path paths = (Path)event.context();	//경로
 					log.info("path = {}", paths.toAbsolutePath()); //C:\...\...\test.txt
 					
 					if(kind.equals(StandardWatchEventKinds.ENTRY_CREATE)) {
 						log.info("created something in directory");
 
-						// 파일처리 함수 호출
+						// 파일처리
 						try {
-							log.info("fnFileCut() call");
-							fnFileCut(paths.getFileName().toString());
+							String[] resultArr = fnFileRead(paths.getFileName().toString());
+							fnFileWrite(paths.getFileName().toString(), resultArr);
 						}catch (IOException e) {
 							e.printStackTrace();
 						}
+
 					}else if(kind.equals(StandardWatchEventKinds.ENTRY_DELETE)) {
 						log.info("delete something in directory");
 					}else if(kind.equals(StandardWatchEventKinds.ENTRY_MODIFY)) {
@@ -74,7 +74,7 @@ public class TickerController
 					}
 				} // end for
 
-
+				// reset
 				if(!watchKey.reset()) {
 					try {
 						log.info("watchService.close()");
@@ -83,7 +83,7 @@ public class TickerController
 						e.printStackTrace();
 					}
 				}
-				log.info("watchService.close()");
+				log.info("================= watchService Thread END =================");
 
 			} // end while
 		}); // end thread
@@ -91,37 +91,71 @@ public class TickerController
 		thread.start();
 	}
 
-	public void fnFileCut(String fileName) throws IOException
+	/**
+	 * 파일 읽기
+	 */
+	public String[] fnFileRead(String fileName) throws IOException
 	{
-		log.info("========== fnFileCut() =============");
-		// 파일 입력스트림 생성
-		String fileFullPath = USER_DIR + "\\" + fileName;
-		log.info("fileFullPath = {}", fileFullPath);
-		FileReader fileReader;
-		try {
-			fileReader = new FileReader(fileFullPath);
-		} catch (FileNotFoundException e) {
-			throw new RuntimeException(e);
+		log.info("========== fnFileCut() ==========");
+		String[] resultArr = new String[1000];
+		String filePath = FILE_LOAD_DIR + "\\" + fileName; //파일경로
+		log.info("filePath = {}", filePath);
+
+		FileInputStream input = new FileInputStream(filePath); //파일 입력스트림 생성
+		InputStreamReader reader = new InputStreamReader(input, "euc-kr");
+		BufferedReader bufferedReader = new BufferedReader(reader); // 입력 버퍼 생성
+
+		String line;
+		while( (line = bufferedReader.readLine()) != null )
+		{ // 파일 내 문자열을 1줄씩 읽기
+			String[] cutStrArr = line.split("\\^");
+			log.info("cutStrArr.length = {}", cutStrArr.length);
+			printArray(cutStrArr);
+
+			resultArr[0] = cutStrArr[14]; //식별자
+			resultArr[1] = cutStrArr[9]; //메시지 내용
+			resultArr[2] = cutStrArr[3]; //반복횟수
+			log.info("resultArr = {}", resultArr[0]);
 		}
 
-		// 입력 버퍼 생성
-		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		bufferedReader.close();
 
-		// 읽기 수행
-		String line ;
+		// TODO
+		// 1.상기 3개정보를 내부 API로 보내줘야 됨. (김정현부장님 검토중)
+		// 2.모든 채널에 재난정보 자막이 나갔다면 "코마"에 결과 전달
+		// 3.완료된 정보 파일로 떨궈줘야 됨.
 
-		// 파일 내 문자열을 1줄씩 읽기
-		while( (line = bufferedReader.readLine()) != null )
-		{
-			log.info("line = {}", line);
-			String[] cutStrArr = line.split("\\^", 20);
-			log.info("cutStrArr.length = {}", cutStrArr.length);
-			log.info("===================");
-			for(String str : cutStrArr)
-			{
-				log.info("  split str = {}", str);
-			}
-			log.info("total cutStr = {}", cutStrArr);
+		return resultArr;
+	}
+
+	/**
+	 * 파일 저장
+	 */
+	public void fnFileWrite(String fileName, String[] dataArr) throws IOException
+	{
+		log.info("========== fnFileWrite() ==========");
+		if (dataArr == null) return;
+
+		// 신규 파일 생성
+		File newFile = new File(FILE_SAVE_DIR + "\\" + "RSLT_" + fileName);
+		log.info("fileInfo = {}", newFile.getPath());
+
+		FileOutputStream fileWriter = new FileOutputStream(newFile, false); //파일 출력스트림 생성
+		OutputStreamWriter writer = new OutputStreamWriter(fileWriter, StandardCharsets.UTF_8);
+		BufferedWriter bufferedWriter = new BufferedWriter(writer); //출력 버퍼 생성
+
+		bufferedWriter.write(dataArr[0]); //저장
+
+		bufferedWriter.flush();
+		bufferedWriter.close();
+	}
+
+	public void printArray(String[] array) {
+		if (array == null)
+			return;
+
+		for(int i = 0; i < array.length; i++) {
+			log.info(" str[{}] = {}", i, array[i]);
 		}
 	}
 
